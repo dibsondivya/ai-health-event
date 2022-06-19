0d8d2ce4d0920010131f35f20fea9bdc1ea37f72
Nicole <nicoleyu@outlook.sg>
2022-06-19 11:59:05 +0800

    Updated flask app for integration

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for, session, make_response, flash
 
+from predict import convert_label, predict
+
 app = Flask(__name__)
 
 
@@ -12,7 +14,10 @@ def output():
     input_text = request.form['input_text']
     ## iterate through series of actions
     label = input_text
-    return jsonify({'htmlresponse' : label})
+    predicted_label = predict(label)
+    predicted_label = convert_label(predicted_label)
+    out = f'The tweet is a(n) {predicted_label.upper()}.'
+    return jsonify({'htmlresponse' : out})
 
 if __name__ == "__main__":  
     app.run(debug=True, port=2000)
